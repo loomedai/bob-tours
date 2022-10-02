82841b152348815899aa4aa2527120cd39d5fedd
loomedai <bbrith@hotmail.com>
2022-10-02 14:06:24 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bob-tours',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

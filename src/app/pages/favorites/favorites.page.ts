c3f200731820db218dd1e457836889e84f3019ec
loomedai <bbrith@hotmail.com>
2022-10-02 15:16:56 +0200

    array tours

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesPage implements OnInit {
 
+  tours = [
+    {ID:1, Title:'City walk'},
+    {ID:2, Title:'On the trails of Beethoven'},
+    {ID:3, Title:'Villa Hammerschmidt'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
